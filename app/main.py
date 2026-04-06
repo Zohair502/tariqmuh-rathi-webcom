681a119fe5962130a19c29b5e02fd114c9bcd3d1
Zohair502 <zohairmzt@gmail.com>
2026-04-06 17:16:03 +0300

    ALLOW CORS

--- app/main.py
+++ app/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI, Request as REQUEST
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = [ "*" ]  # Allow all origins for development; restrict in production
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.get("/")
 def read_root():
