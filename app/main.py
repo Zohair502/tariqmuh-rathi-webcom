a0530625245362e29034d06c790f6006f0ac8141
Zohair502 <zohairmzt@gmail.com>
2026-04-06 15:28:25 +0300

    WHAT IS MY IP

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request as REQUEST
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return { "msg": "Hello docker"}
+    return { "msg": "Hello docker dev mode"}
 
 
-@app.get("/items/{id}")
-def read_item(item_id: int, q: str = None):
-    return {"id": id, "q": q}
+# what is my ip address?
+@app.get("/api/ip")
+def api_ip(request : REQUEST):
+    return { "ip": request.client.host }
+
+@app.get("/ip", response_class=HTMLResponse)
+def ip(request : REQUEST):
+    return f"""
+    <html>
+        <head>
+            <title>My IP Address</title>
+        </head>
+        <body>
+            <h1>Your IP Address is: {request.client.host}</h1>
+        </body>
+    </html>
+    """
